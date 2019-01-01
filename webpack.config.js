69f2a0c9f589483278f2993b0f6676e999a6f455
Dima-macbook <dima@virtuman.com>
2019-01-02 02:10:38 +0300

    added bootstrapped files to create propper template for fast starting

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require('path');
 
+
+// Phaser webpack config
+const phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
 module.exports = {
     mode: "development",
-    entry: "./app",
+    entry: "./src",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
     },
     context: __dirname,
-    devtool : 'source-map',
+    devtool: 'source-map',
     devServer: {
-        contentBase: 'dist',
+        contentBase: 'static',
+        publicPath: 'dist',
         port: 9005,
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: ['babel-loader'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /phaser-split\.js$/,
+                use: ['expose-loader?Phaser']
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            'phaser': phaser,
+        }
     }
 };
